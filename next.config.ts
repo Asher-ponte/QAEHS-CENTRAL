4375b6508edf804309b0734ff7e1af507b515cf1
Florante Catapang <florantecatapang6@gmail.com>
2025-06-23 09:51:55 +0000

    make an index.hmtl as main app

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
