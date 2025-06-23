779af3697b530c076fadbbac1c45de81548332d4
Florante Catapang <florantecatapang6@gmail.com>
2025-06-23 09:44:57 +0000

    please try again is not wokring make sure 2 column grid

--- src/components/app-links.tsx
+++ src/components/app-links.tsx
@@ -38,7 +38,7 @@ const appLinks: AppLink[] = [
 
 export function AppLinks() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="grid grid-cols-2 gap-4">
       {appLinks.map((link) => (
         <a
           key={link.title}
